11071e04d86e8c4172aac004bdd9601622029e00
hyeladzirajames <adzirajbalami@gmail.com>
2025-02-16 19:39:18 +0000

    Onboarding

---
+++ lib/features/authentication/controllers/onboarding_controller.dart
@@ -0,0 +1,17 @@
+
+import 'package:get/get.dart';
+
+class OnboardingController extends GetxController{
+  static OnboardingController get instance => Get.find();
+
+  void updatePageIndicator (index) {}
+
+  void dotNavigationClick (index) {}
+
+  void nextPage () {}
+
+  void skipPage () {}
+
+
+}
+
